852dc10977ce228740f749bcdb984c099def3e8c
Kenny Lh <kenny@intellicloud.com>
2023-03-29 23:02:26 -0400

    feat: router, nav bar added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import router from './router'
 
 Vue.config.productionTip = false
+const NotFound = { template: '<p>Page not found</p>' }
+const Home = { template: '<p>home page</p>' }
+const About = { template: '<p>about page</p>' }
+
+const routes = {
+  '/': Home,
+  '/about': About
+}
 
 new Vue({
+  el: '#app',
+
+  data: {
+    currentRoute: window.location.pathname
+  },
+
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
